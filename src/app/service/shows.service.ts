6916d2b5815b3824fbea388efc1b95c4b5b64c9c
Tabassum <sana.tabassum@capgemini.com>
2020-09-08 12:45:01 +0530

    Tv Shows app commit

---
+++ src/app/service/shows.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShowsService {
+
+  //baseURL for the api calls
+  baseURL = 'http://api.tvmaze.com';
+
+  dramaShows = {};
+  actionShows = {};
+  romanceShows = {};
+  sciFiShows = {};
+  horrorShows = {};
+
+  constructor(private http: HttpClient) { }
+
+  // Fetches all available shows data.
+  getAllShows(): Observable<[]>{
+    return this.http.get(`${this.baseURL}/shows`)
+    .pipe(map((data: any) => data));
+  }
+
+  // Fetches show data by id.
+  getShowById(id: number): Observable<{}>{
+    return this.http.get(`${this.baseURL}/shows/${id}`);
+  }
+
+  // Fetches season list for given show.
+  getShowSeasonList(id: number): Observable<[]>{
+    return this.http.get(`${this.baseURL}/shows/${id}/seasons`)
+    .pipe(map((data: any) => data));
+  }
+
+  // Searches data based on search string.
+  search(term: string): Observable<[]> {
+    if(!term){
+      return of(null);
+    }
+    return this.http.get(`${this.baseURL}/search/shows?q=${term}`)
+    .pipe(map((data: any) => data));
+  }  
+}
---
+++ src/app/show-details/show-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { ShowsService } from '../service/shows.service';
+
+@Component({
+  selector: 'app-show-details',
+  templateUrl: './show-details.component.html',
+  styleUrls: ['./show-details.component.scss']
+})
+export class ShowDetailsComponent implements OnInit {
+
+  isLoaded = false;
+  showId = 0;
+  showDetails = {};
+  showSeasonList = [];
+
+  constructor(
+    private actRoute: ActivatedRoute, 
+    private service: ShowsService, 
+    public location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.actRoute.params.subscribe(params => {
+      this.showId = params['id'];
+      this.service.getShowById(this.showId).subscribe(data => {
+        this.isLoaded = true;
+        this.showDetails = data;
+      });
+      this.service.getShowSeasonList(this.showId).subscribe(data => {
+        this.showSeasonList = data;
+      });
+    });
+  }
+
+  // Handles back button click in the details page.
+  // Navigates user to previous screen based on history.
+  goBack(): void{
+    this.location.back();
+  }
+
+}
